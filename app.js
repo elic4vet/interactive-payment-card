7e16ea423b5b065160f334db179a5182de604a99
Elisabeth <elise.doisneau@gmail.com>
2024-03-15 19:56:36 +0100

    Update app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+const elements = {
+    cardHolderName: {
+        input: document.querySelector("#cardholder-name"),
+        text: document.querySelector(".card__owner")
+    },
+    cardNumber: {
+        input: document.querySelector("#card-number"),
+        text: document.querySelector(".card__number")
+    },
+    expiracyMonth: {
+        input: document.querySelector("#mm"),
+        text: document.querySelector(".expiracy__date")
+    },
+    expiracyYear: {
+        input: document.querySelector("#yy"),
+        text: document.querySelector(".expiracy__date")
+    },
+    cvcCode: {
+        input: document.querySelector("#cvc"),
+        text: document.querySelector(".cvc__text")
+    }
+};
+
+
+function updateTextContent(inputElement, textElement) {
+    textElement.textContent = inputElement.value;
+}
+
+Object.values(elements).forEach(element => {
+    element.input.addEventListener("input", () => {
+        updateTextContent(element.input, element.text);
+    });
+});
